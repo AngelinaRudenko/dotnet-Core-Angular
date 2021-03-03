91f197b07da18b20c8aa05b93255d620d3b3f63b
Angelina Rudenko <angel.rudenko.777@gmail.com>
2021-03-03 17:14:25 +0300

    initial commit

---
+++ ParentApi/Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using ParentApi.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ParentApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        public List<User> users { get; set; }
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+
+        ///GET .../users
+        [HttpGet]
+        public async Task<Users> GetAsync()
+        {
+            WebRequest request = WebRequest.Create("https://dummyapi.io/data/api/user");
+            request.Method = "GET";
+            request.Headers["app-id"] = "603f7a4dbe13b16adaa19d17";
+
+            WebResponse response = await request.GetResponseAsync();
+            string str = "";
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    str = reader.ReadToEnd();
+                }
+            }
+            response.Close();
+
+            //JSON deserialization
+            Users myDeserializedClass = JsonConvert.DeserializeObject<Users>(str);
+
+            return myDeserializedClass;
+        }
+    }
+}
---
+++ ParentApi/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace ParentApi.Models
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    // according to https://json2csharp.com
+
+    public class User
+    {
+        public string id { get; set; }
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+        public string email { get; set; }
+        public string title { get; set; }
+        public string picture { get; set; }
+    }
+
+    public class Users
+    {
+        public List<User> data { get; set; }
+        public int total { get; set; }
+        public int page { get; set; }
+        public int limit { get; set; }
+        public int offset { get; set; }
+    }
+}
